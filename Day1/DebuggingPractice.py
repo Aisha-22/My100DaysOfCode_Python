edb3cb692c5c6caadecbce9e0218fe4d8986b052
Aishia <51677946+Aisha-22@users.noreply.github.com>
2022-01-19 22:57:07 +0200

    Day2

---
+++ Day1/DebuggingPractice.py
@@ -0,0 +1,12 @@
+# Example Output
+# When you run your program, it should print the following:
+#
+# Day 1 - String Manipulation
+# String Concatenation is done with the "+" sign.
+# e.g. print("Hello " + "world")
+# New lines can be created with a backslash and n.
+
+# Fix the code below 👇
+
+print('Day 1 - String Manipulation\nString Concatenation is done with the "+" sign.\ne.g. print("Hello " + '
+      '"world")\nNew lines can be created with a backslash and n.')
---
+++ Day1/InputFunction.py
@@ -0,0 +1,8 @@
+# Example Input
+# Angela
+# Example Output
+# 6
+
+# Write your code below this line 👇
+
+print(len(input("What is your name?")))
