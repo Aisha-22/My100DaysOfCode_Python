14a481fd4fad2f79fbf8f39b55164f6a92eb705b
Aishia <51677946+Aisha-22@users.noreply.github.com>
2022-01-20 23:41:01 +0200

    Day3

---
+++ Day2/DataTypes.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+
+# Type checking and Type conversion
+
+# This variable will take the fist number of the two_digit_number through the input function
+first_digit = two_digit_number[0]
+
+# This variable will take the second number of the two_digit_number through the input function
+second_digit = two_digit_number[1]
+
+# These functions will calculate the numbers by adding the two_digit_number together and will be assigned to the
+# variable 'result'
+result = int(first_digit) + int(second_digit)
+
+# This print function will print the result
+print(result)
+
