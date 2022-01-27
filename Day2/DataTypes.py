2636860593cc64beb2f7e0a33c9d8fcfe0ab5687
Aishia <51677946+Aisha-22@users.noreply.github.com>
2022-01-27 08:25:45 +0200

    Day2-Challenge

--- Day2/DataTypes.py
+++ Day2/DataTypes.py
@@ -7,10 +7,12 @@ two_digit_number = input("Type a two digit number: ")
 
 # Type checking and Type conversion
 
-# This variable will take the fist number of the two_digit_number through the input function
+# This variable will take the fist digit at the position [0] of the two_digit_number through the input function from
+# the user/users keyboard
 first_digit = two_digit_number[0]
 
-# This variable will take the second number of the two_digit_number through the input function
+# This variable will take the second at the position [1] of the two_digit_number through the input function from
+# # the user/users keyboard
 second_digit = two_digit_number[1]
 
 # These functions will calculate the numbers by adding the two_digit_number together and will be assigned to the
@@ -20,3 +22,12 @@ result = int(first_digit) + int(second_digit)
 # This print function will print the result
 print(result)
 
+#  Aisha - Prac
+
+name = "Aisha"
+
+print(f"The letter at position 0 for Aisha = {name[0]}")
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
---
+++ Day2/LifeInWeeks.py
@@ -0,0 +1,23 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+# Change age into a number
+age_as_int = int(age)
+
+# Calculate number of years left
+year_remaining = 100 - age_as_int
+
+# Calculate number of Days left
+days_remaining = year_remaining * 365
+
+# Calculate number of weeks left
+weeks_remaining = year_remaining * 52
+
+# Calculate number of Months left
+months_remaining = year_remaining * 12
+
+message = f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left."
+print(message)
