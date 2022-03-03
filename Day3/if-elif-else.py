63e12e9faf4083ccaa3c2f5e47d4ef9870ec8225
Aishia <51677946+Aisha-22@users.noreply.github.com>
2022-03-03 07:08:21 +0200

    If-Elif-Else Statements

---
+++ Day3/if-elif-else.py
@@ -0,0 +1,14 @@
+print("Welcome to the roller coaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the roller coaster")
+    age = int(input("What is your age"))
+    if age < 12:
+        print("Please pay R5 Ticket")
+    elif age <= 18:
+        print("Please pay R7 Ticket")
+    else:
+        print("Please pay R12 Ticket")
+else:
+    print("Sorry - Wait till your height is 120 or over")
